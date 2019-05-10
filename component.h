b0c2170bd87ed11ae400cfb099019cfddf3032c8
EhsanS <e.escandari1@gmail.com>
2019-05-10 09:30:55 +0430

    add component.h

---
+++ component.h
@@ -0,0 +1,18 @@
+
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include "sources.h"
+
+class Component
+{
+public:
+  Component(TYPE_NAME tp);
+  virtual bool validation() const = 0;
+  virtual bool operator<(const Component& cp) const ;
+  virtual bool operator==(const Component& cp) const ;
+private:
+  TYPE_NAME type;
+};
+
+#endif
