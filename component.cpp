ccd9813d46460df9e767665a955d8cf60faadeba
EhsanS <e.escandari1@gmail.com>
2019-05-10 13:45:23 +0430

    add operator to components

--- component.cpp
+++ component.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 Component::Component(TYPE_NAME tp){ type = tp; }
 TYPE_NAME Component::get_type() { return type; }
-// virtual bool Component::operator<(const Component& cp) const { return true; }
-// virtual bool Component::operator==(const Component& cp) const { return true; }
+
+bool Component::operator<(const Component& cp) const
+{
+  cout << "Operator is called " << get_value() << " | " << cp.get_value() << endl;
+  if(get_value()[0]>='0' && get_value()[0]<='9')
+    return stoi(get_value()) < stoi(cp.get_value());
+  return get_value() < cp.get_value();
+}
+
+bool Component::operator==(const Component& cp) const
+{
+  return get_value() == cp.get_value();
+}
--- component.h
+++ component.h
@@ -10,8 +10,9 @@ public:
   Component(TYPE_NAME tp);
   TYPE_NAME get_type();
   virtual bool validation() const = 0;
-  // virtual bool operator<(const Component& cp) const ;
-  // virtual bool operator==(const Component& cp) const ;
+  bool operator<(const Component& cp) const ;
+  bool operator==(const Component& cp) const ;
+  virtual std::string get_value() const = 0;
 private:
   TYPE_NAME type;
 };
