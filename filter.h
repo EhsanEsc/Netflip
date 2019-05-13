60fb155bace124bb3af3fd5db0d1a9f7455b794e
EhsanS <e.escandari1@gmail.com>
2019-05-14 01:37:20 +0430

    add sort algorithm

--- filter.h
+++ filter.h
@@ -24,6 +24,20 @@ namespace Filter_interface
         res.push_back(u);
     return res;
   }
+  template<typename T> vector<T*> sort(vector<T*> vt, TYPE_NAME tn)
+  {
+    // bubble sort
+    for(int i=0;i<int(vt.size())-1;i++)
+    {
+      for(int j=0;j<int(vt.size())-i-1;j++)
+      {
+        cout << i << " " << j << endl;
+        if(*(vt[j+1]->get_component22(tn))<*(vt[j]->get_component22(tn)))
+          swap(vt[j],vt[j+1]);
+      }
+    }
+    return vt;
+  }
   // template User* find_exact<User>(vector<User*> vt, Component* cp);
 }
 
